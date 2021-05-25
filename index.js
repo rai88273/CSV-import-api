aff3fec427d6da190ae7a5f65929ef9441697079
rai88273 <46261438+rai88273@users.noreply.github.com>
2021-05-25 19:06:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -105,6 +105,6 @@ function isLoggedIn(req, res, next) {
 // Configuring server to listen on port 3000
 // ******************************
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is now running !!");
 });
